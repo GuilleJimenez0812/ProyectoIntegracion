58733855520ad40bffc91bd2afca38bfaf91935a
giovax11 <giovax1110@gmail.com>
2023-06-11 15:28:58 -0400

    bugfix

--- src/entidades/entidades.module.ts
+++ src/entidades/entidades.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { EntidadesController } from './entidades.controller';
 import { EntidadesService } from './entidades.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { Sucursal } from './sucursal.entity';
+import { metodo_pago } from './metodos_pago.entity';
+import { modelo_vehiculo } from './modelo_vehiculos.entity';
 
 @Module({
   controllers: [EntidadesController],
-  providers: [EntidadesService]
+  providers: [EntidadesService],
+  imports: [
+    TypeOrmModule,
+    TypeOrmModule.forFeature([metodo_pago, modelo_vehiculo, Sucursal]),
+  ],
 })
 export class EntidadesModule {}
