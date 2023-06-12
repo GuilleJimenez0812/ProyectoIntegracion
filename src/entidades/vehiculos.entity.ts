52e4095c150c11a4ab1b59b08a4db8e24c33a4e1
giovax11 <giovax1110@gmail.com>
2023-06-12 16:04:54 -0400

    vale

--- src/entidades/vehiculos.entity.ts
+++ src/entidades/vehiculos.entity.ts
@@ -1,28 +1,28 @@
-import { Column, Entity } from "typeorm";
+import { Column, Entity } from 'typeorm';
 
 @Entity()
 class Vehiculo {
-    @Column()
-    id_vehiculo: number
+  @Column()
+  id_vehiculo: number;
 
-    @Column()
-    marca: string
-    
-    @Column()
-    modelo: string
+  @Column()
+  marca: string;
 
-    @Column()
-    anio: string
+  @Column()
+  modelo: string;
 
-    @Column()
-    tipo_vehiculo: string
+  @Column()
+  anio: string;
 
-    @Column()
-    caracteristicas: string
+  @Column()
+  tipo_vehiculo: string;
 
-    @Column()
-    estado_vehiculo: string
+  @Column()
+  caracteristicas: string;
 
-    @Column()
-    precio_alquiler_por_dia: number
+  @Column()
+  estado_vehiculo: string;
+
+  @Column()
+  precio_alquiler_por_dia: number;
 }
